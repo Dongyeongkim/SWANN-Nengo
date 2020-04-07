7e5204c254b0a58bc68202f8a85c543d6a6f3b8f
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-04-07 18:24:46 +0900

    Add files via upload

---
+++ NEAT.py
@@ -0,0 +1,120 @@
+import time
+import numpy as np
+import random as rd
+
+
+def translate_gene_into_nengo_param(gene_pool):
+    gene_pool_list = []
+    for gene in gene_pool:
+        gene_node_list = []
+        for nuc in gene:
+            gene_node_list.append(nuc[0])
+            gene_node_list.append(nuc[1])
+        gene_node_list.sort()
+        gene_connection_list = gene.copy()
+        gene_pool_list.append([gene_node_list, gene_connection_list])
+        f = open('gene/'+str(time.strftime('%c', time.localtime(time.time())))+'.txt', 'w')
+        f.write(str(gene_pool_list))
+        f.close()
+
+    return gene_pool_list
+
+
+def generate_first_generation(gene_pool_size, input_neuron, output_neuron):
+    gene_pool_list = []
+    for _ in range(gene_pool_size):
+        gene_list = []
+        gene_list.append([rd.randint(0, input_neuron - 1), rd.randint(0, output_neuron - 1)])
+        connecting_num = rd.randint(1, input_neuron * output_neuron)
+        for _ in range(connecting_num):
+            rand = [rd.randint(0, input_neuron - 1), rd.randint(0, output_neuron - 1)]
+            gene_list.append(rand)
+        gene_tuple = set([tuple(gene_list) for gene_list in gene_list])
+        gene_list = []
+        for gene in gene_tuple:
+            gene_list.append([gene[0], gene[1]])
+
+        gene_pool_list.append(gene_list)
+
+    return gene_pool_list
+
+
+def mutate(gene_pool, adding_node_probability, adding_connection_probability):
+    gene_pool_list = []
+    mutated_gene_pool_list = []
+    for gene in gene_pool:
+        gene_node_list = []
+        for nuc in gene:
+            gene_node_list.append(nuc[0])
+            gene_node_list.append(nuc[1])
+        gene_node_list = list(set(gene_node_list))
+        gene_node_list.sort()
+        gene_connection_list = gene.copy()
+        gene_pool_list.append([gene_node_list, gene_connection_list])
+    for gene_info in gene_pool_list:
+        node_mutated_gene = []
+        current_num = gene_info[0][-1]
+        for i in gene_info[1]:
+            if rd.random() < adding_node_probability:
+                node_mutated_gene.append([i[0], current_num + 1])
+                node_mutated_gene.append([current_num + 1, i[1]])
+            else:
+                node_mutated_gene.append(i)
+        fully_mutated_gene = node_mutated_gene.copy()
+        mnuc_node_list = []
+        for mnuc in node_mutated_gene:
+            mnuc_node_list.append(mnuc[0])
+            mnuc_node_list.append(mnuc[1])
+        mnuc_node_list = list(set(mnuc_node_list))
+        if rd.random() < adding_connection_probability:
+            try:
+                while True:
+                    rand_connection = rd.sample(mnuc_node_list, 2)
+                    fully_mutated_gene.index(rand_connection)
+            except ValueError:
+                fully_mutated_gene.append(rand_connection)
+        else:
+            pass
+        mutated_gene_pool_list.append(fully_mutated_gene)
+    return mutated_gene_pool_list
+
+
+def crossover(gene_pool, prob_list):
+    check_list = list(range(len(gene_pool)))
+    checked = np.random.choice(check_list, size=len(gene_pool), p=prob_list)
+
+    adapted_genes = []
+    for check in checked:
+        adapted_genes.append(gene_pool[check])
+    rd.shuffle(adapted_genes)
+    choiced_list = []
+    crossovered_gene_pool_list = []
+    for i in range(int(len(gene_pool) / 2)):
+        choiced_list.append(adapted_genes[2 * i:2 * i + 2])
+
+    for choiced in choiced_list:
+        gene1 = []
+        gene2 = []
+        check_mark = []
+        check_mark_2 = []
+        for i in range(len(choiced[0])):
+            if rd.randint(0, 2) == 0:
+                check_mark.append(0)
+            else:
+                check_mark.append(1)
+        for i in range(len(choiced[0])):
+            if rd.randint(0, 2) == 0:
+                check_mark_2.append(1)
+            else:
+                check_mark_2.append(0)
+        for i, indic in enumerate(check_mark):
+            gene1.append(choiced[indic][i])
+        for j, indicc in enumerate(check_mark_2):
+            gene2.append(choiced[indicc][j])
+        crossovered_gene_pool_list.append([gene1])
+        crossovered_gene_pool_list.append([gene2])
+    return crossovered_gene_pool_list
+
+
+
+
---
+++ main.py
@@ -0,0 +1,128 @@
+import gym
+import nengo
+import nengo_ocl
+import numpy as np
+import NEAT
+
+
+
+env = gym.make('CartPole-v0').env
+
+class EnvironmentInterface(object):
+    def __init__(self, env, stepSize=5):
+        self.env = env
+        self.n_actions = env.action_space.n
+        self.state_dim = env.observation_space.shape[0]
+        self.t = 0
+        self.stepsize = stepSize
+        self.output = np.zeros(self.n_actions)
+        self.state = env.reset()
+        self.reward = 0
+        self.current_action = 0
+        self.reward_arr = []
+        self.totalReward = 0
+
+    def take_action(self, action):
+        self.state, self.reward, self.done, _ = env.step(action)
+        self.totalReward += self.reward
+        if self.done:
+            self.reward = -2
+            self.totalReward += self.reward
+            self.reward_arr.append(self.totalReward)
+            self.state = env.reset()
+            self.totalReward = 0
+
+    def get_reward(self, t):
+        return self.reward
+
+    def sensor(self, t):
+        return self.state
+
+    def step(self, t, x):
+        if int(t * 1000) % self.stepsize == 0:
+            self.current_action = np.argmax(x)  # np.argmax(self.output)#
+            self.take_action(self.current_action)
+
+tau = 0.01
+
+fast_tau = 0
+slow_tau = 0.01
+
+n_action =2
+
+envI = EnvironmentInterface(env)
+
+state_dimensions = envI.state_dim
+n_actions = envI.n_actions
+
+from gym import wrappers
+from datetime import datetime
+#Video Capturing Mechanism
+filename="test"
+is_monitor=False
+# env.close()
+if is_monitor:
+    #filename = os.path.basename(__file__).split('.')[0]
+    monitor_dir = './' + filename + '_' + str(datetime.now())
+    env = wrappers.Monitor(env, monitor_dir)
+    env.reset()
+
+Gen = int(input('Generation_number'))
+inp = int(input('please input input size'))
+outp = int(input('please input output size'))
+
+for i in range(Gen):
+    if i == 0:
+        gene_list = NEAT.generate_first_generation(96,4,2).copy()
+        translated = NEAT.translate_gene_into_nengo_param(gene_list)
+    else:
+        gene_list = NEAT.crossover(gene_list,score_list)
+        gene_list = NEAT.mutate(gene_list,0.25,0.25)
+        translated = NEAT.translate_gene_into_nengo_param(gene_list)
+    score_list = []
+    for n in translated:
+        node = n[0]
+        connection = n[1]
+        model = nengo.Network()
+        with model:
+            all_neuron = int(n[0][-1] + 1)
+            sensor_nodes = nengo.Node(envI.sensor)
+            sensing_neuron = nengo.Ensemble(n_neurons=envI.state_dim,dimensions=envI.state_dim,
+                                            neuron_type=nengo.Izhikevich())
+            action_neurons = nengo.Ensemble(n_neurons=envI.n_actions, dimensions=envI.n_actions,
+                                            neuron_type=nengo.Izhikevich())
+            nengo.Connection(sensor_nodes,sensing_neuron.neurons)
+            middle_neurons = {}
+
+            for f in node:
+                if f < inp-1:
+                    pass
+                elif f < outp-1:
+                    pass
+                else:
+                    middle_neurons[f] = nengo.Ensemble(1, dimensions=1, neuron_type=nengo.Izhikevich())
+            for k in connection:
+                if k[0] < inp-1:
+                    if k[1] < outp-1:
+                        nengo.Connection(sensing_neuron.neurons[k[0]], action_neurons[k[1]], synapse=fast_tau)
+                    else:
+                        nengo.Connection(sensing_neuron[k[0]], middle_neurons[k[1]], synapse=tau)
+                else:
+                    if k[1] < outp-1:
+                        nengo.Connection(middle_neurons[k[0]], action_neurons[k[1]], synapse=tau)
+                    else:
+                        nengo.Connection(middle_neurons[k[0]], action_neurons[k[0]], synapse=tau)
+        simulator = nengo_ocl.Simulator(model)
+        for _ in range(200000):
+            simulator.step()
+            reward = envI.get_reward()
+            print(reward)
+
+
+
+
+
+
+
+
+
