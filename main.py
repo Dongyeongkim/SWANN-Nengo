92a84be7563f07951644f62481e0d48d41c4a8ad
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-04-07 23:27:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import NEAT
 
 env = gym.make('CartPole-v0').env
 
+
 class EnvironmentInterface(object):
     def __init__(self, env, stepSize=5):
         self.env = env
@@ -41,26 +42,23 @@ class EnvironmentInterface(object):
             self.current_action = np.argmax(x)  # np.argmax(self.output)#
             self.take_action(self.current_action)
 
+
 tau = 0.01
 
 fast_tau = 0
 slow_tau = 0.01
 
-n_action =2
 
-envI = EnvironmentInterface(env)
-
-state_dimensions = envI.state_dim
-n_actions = envI.n_actions
 
 from gym import wrappers
 from datetime import datetime
-#Video Capturing Mechanism
-filename="test"
-is_monitor=False
+
+# Video Capturing Mechanism
+filename = "test"
+is_monitor = False
 # env.close()
 if is_monitor:
-    #filename = os.path.basename(__file__).split('.')[0]
+    # filename = os.path.basename(__file__).split('.')[0]
     monitor_dir = './' + filename + '_' + str(datetime.now())
     env = wrappers.Monitor(env, monitor_dir)
     env.reset()
@@ -70,29 +68,34 @@ prob_list = []
 for i in range(Gen):
     score_list = []
     if i == 0:
-        gene_list = NEAT.generate_first_generation(96,4,2).copy()
+        gene_list = NEAT.generate_first_generation(10, 4, 2).copy()
+        print(gene_list)
         translated = NEAT.translate_gene_into_nengo_param(gene_list)
     else:
-        gene_list = NEAT.crossover(gene_list,prob_list)
-        gene_list = NEAT.mutate(gene_list,0.25,0.25)
+        gene_list = NEAT.crossover(gene_list, prob_list)
+        print(gene_list)
+        gene_list = NEAT.mutate(gene_list, 0.25, 0.25)
         translated = NEAT.translate_gene_into_nengo_param(gene_list)
+        print(translated)
         score_list = []
         prob_list = []
- 
+
     for n in translated:
+        envI = EnvironmentInterface(env)
+        state_dimensions = envI.state_dim
+        n_actions = envI.n_actions
         node = n[0]
         connection = n[1]
         model = nengo.Network()
         with model:
-            all_neuron = int(n[0][-1] + 1)
             sensor_nodes = nengo.Node(envI.sensor)
-            sensing_neuron = nengo.Ensemble(n_neurons=envI.state_dim,dimensions=envI.state_dim)
+            sensing_neuron = nengo.Ensemble(n_neurons=envI.state_dim, dimensions=envI.state_dim)
             action_neurons = nengo.Ensemble(n_neurons=envI.n_actions, dimensions=envI.n_actions)
-            step_node = nengo.Node(envI.step,size_in=2)
-            nengo.Connection(action_neurons,step_node,synapse=fast_tau)
-            nengo.Connection(sensor_nodes,sensing_neuron.neurons)
+            step_node = nengo.Node(envI.step, size_in=2)
+            nengo.Connection(action_neurons, step_node, synapse=fast_tau)
+            nengo.Connection(sensor_nodes, sensing_neuron.neurons)
             middle_neurons = {}
-
+            node = list(set(node)); print(node)
             for f in node:
                 if f < envI.state_dim:
                     pass
@@ -103,25 +106,27 @@ for i in range(Gen):
             for k in connection:
                 if k[0] < envI.state_dim:
                     if k[1] < envI.n_actions:
-                        nengo.Connection(sensing_neuron.neurons[k[0]], action_neurons[k[1]], synapse=fast_tau)
+                        nengo.Connection(sensing_neuron.neurons[k[0]], action_neurons.neurons[k[1]], synapse=fast_tau)
+                    elif envI.n_actions <= k[1] < envI.state_dim:
+                        nengo.Connection(sensing_neuron.neurons[k[0]],sensing_neuron.neurons[k[1]], synapse=tau)
                     else:
-                        nengo.Connection(sensing_neuron[k[0]], middle_neurons[k[1]], synapse=tau)
+                        nengo.Connection(sensing_neuron.neurons[k[0]], middle_neurons[k[1]], synapse=tau)
                 else:
                     if k[1] < envI.n_actions:
-                        nengo.Connection(middle_neurons[k[0]], action_neurons[k[1]], synapse=tau)
+                        nengo.Connection(middle_neurons[k[0]], action_neurons.neurons[k[1]], synapse=tau)
+                    elif envI.n_actions <= k[1] < envI.state_dim:
+                        nengo.Connection(middle_neurons[k[0]], sensing_neuron.neurons[k[1]], synapse=tau)
                     else:
-                        nengo.Connection(middle_neurons[k[0]], action_neurons[k[0]], synapse=tau)
+                        nengo.Connection(middle_neurons[k[0]], middle_neurons[k[1]], synapse=tau)
         simulator = nengo_ocl.Simulator(model)
-        a = 200000
         with nengo_ocl.Simulator(model) as sim:
-            sim.run(90.0)
-        print("Reward:"+str(sum(envI.reward_arr)))
+            sim.run(3.0)
+        print("Reward:" + str(sum(envI.reward_arr)))
         score_list.append(sum(envI.reward_arr))
-
+        print(score_list)
     sum_score = sum(score_list)
     for z in score_list:
-        prob_list.append(z/sum_score)
-
+        prob_list.append(z / sum_score)
 
 
 
