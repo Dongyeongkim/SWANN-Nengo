c015e3e79b9fa82b586b684aa4609099b3b536ac
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-05-23 23:13:21 +0900

    Update main_multi.py

--- main_multi.py
+++ main_multi.py
@@ -62,7 +62,7 @@ def sim(n):
             with nengo_dl.Simulator(model,device="/gpu:0") as sim:
                 sim.run_steps(200)
                 avg_score_list = average(np.array(envI.reward_arr))
-                sco_var_env.append(np.sum(avg_score_list) / (len(avg_score_list) * len(n[1])))
+                sco_var_env.append(np.sum(avg_score_list)/len(avg_score_list)-len(n[1]))
 
         except nengo.exceptions.BuildError:
             sco_var_env.append(0)
